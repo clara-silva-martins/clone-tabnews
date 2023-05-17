3c1851a9afea4889461ca4dd70cf7dded5c85774
Clara Martins <95709670+clara-silva-martins@users.noreply.github.com>
2023-05-17 18:42:09 +0000

    adiciona arquivo pages

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Oi, meu amor essa mensagem é para você! Feliz dia 18 hoje completamos 1 ano e 5 meses! Te amo imensamente, cada pedacinho seu é algo delicioso, sua voz, risada, seu abraço, me sinto muito confortável em cada pedacinho desse esparramada abraçando cada pedaço como quem agarra micro planetinhas flutuantes e doces, eu te amo infinitamente. Obrigada por todo incentivo, meu planetinha, meu amor, meu lar, meu grande amigo e parceiro. </h1>
+}
+
+export default Home;
