7ebce51080c99d14a0f46b1b662b60204b47f45f
Clara Martins <95709670+clara-silva-martins@users.noreply.github.com>
2023-05-18 14:31:54 +0000

    gif

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Feliz dia 18 cabeçudo! Parabéns por mais uma conquista primeiro passo pra construir nossa futura 🏠 🚌 </h1>
+    return <h1>Feliz dia 18 cabeçudo! Parabéns por mais uma conquista primeiro passo pra construir nossa futura 🏠 🚌 
+        <iframe src="https://giphy.com/embed/yHIsnPl1khoU9Ms0Xe" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/HallmarkChannel-meet-the-peetes-mtp-0303-yHIsnPl1khoU9Ms0Xe">via GIPHY</a></p>
+    </h1>
 }
 
 export default Home;
