080829cb2ddd77529c01b482905b918d538808b1
Clara Martins <95709670+clara-silva-martins@users.noreply.github.com>
2023-05-18 14:24:39 +0000

    correção do commit anterior

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Feliz dia 18 cabeçudo! Parábens por mais uma conquista primeiro passo pra construir nossa futura &#127969 &#	128654 </h1>
+    return <h1>Feliz dia 18 cabeçudo! Parabéns por mais uma conquista primeiro passo pra construir nossa futura &#127969 &#	128654 </h1>
 }
 
 export default Home;
